3e3c51629463f93d263a3359a1ca1a380c47c952
Ben Muller <benmuller@Bens-Macbook.modem>
2019-05-08 18:03:19 +1000

    fixed issue

--- db/migrate/20190508080207_remove_airplane_id_from_flight.rb
+++ db/migrate/20190508080207_remove_airplane_id_from_flight.rb
@@ -1,5 +1,5 @@
 class RemoveAirplaneIdFromFlight < ActiveRecord::Migration[5.2]
   def change
-    remove_column :flights, :airplane_id, :integer
+    remove_column :flights, :airplane_id, :text
   end
 end
