27a4b5d97de9e6127f191d6aacde64a26b1211c7
Ben Muller <benmuller@Bens-Macbook.local>
2019-05-09 13:32:55 +1000

    integrating with github

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -11,8 +11,7 @@ class SessionController < ApplicationController
   session[:user_id] = user.id
   session[:admin] = user.admin
   session[:username] = user.username
-      
-    redirect_to "http://localhost:3001/?#{ user.username }", :overwrite_params => { :parm => 'foo' }
+    redirect_to "ttps://samseabourn.github.io/team5onfire-client/?#{ user.username }", :overwrite_params => { :parm => 'foo' }
 
   # redirect_to flights_path
       
