6060f31215caeb7697038c46eda0adaf7c2a8c80
Ben Muller <benmuller@Bens-Macbook.local>
2019-05-09 12:27:14 +1000

    added username to reservation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_08_080241) do
+ActiveRecord::Schema.define(version: 2019_05_09_022635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_05_08_080241) do
     t.datetime "updated_at", null: false
     t.text "flight_id"
     t.text "user_id"
+    t.text "username"
   end
 
   create_table "users", force: :cascade do |t|
